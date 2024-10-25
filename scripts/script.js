ac3f7084b4159caa3ad25a7dff2f1cb0b7015ab5
Maaz Gundagi <mgundagi.01@gmail.com>
2024-10-25 06:23:24 -0500

    find one by one algo on array

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 console.log("Hello, World!")
+
+//Array
+const students =['michael', 'john', 'eric', 'jonas', 'karen', 'olsen', 'jackey','michael']
+//One by One Find Algorithm
+const searchStudent = (studentName) => {
+    let didFind = false;
+    for(i = 0; i < students.length; i++) {
+        let marker = ' '
+        if(students[i]==studentName){
+            didFind = true;
+            marker = ' < match found'
+        }
+        console.log('loop executed time:' + i + marker)
+    }
+    console.log(`Student was found?: ${didFind}`)
+}
+searchStudent('eric')
